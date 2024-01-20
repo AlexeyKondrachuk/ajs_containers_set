983ddeb2faa1fabc5f0c30f7cc6403eb639977e7
Alexey.K <lexxtlt@gmail.com>
2024-01-20 13:07:51 +0400

    correct test

--- src/js/__tests__/team_test_2.js
+++ src/js/__tests__/team_test_2.js
@@ -2,15 +2,7 @@ import Team from '../app';
 
 const first = { character: 'character_1' };
 const second = { character: 'character_2' };
-const doubleFirst = first;
-const lectorObject = {
-  name: 'Лучник',
-  type: 'Bowman',
-  health: 50,
-  level: 3,
-  attack: 40,
-  defence: 10,
-};
+
 
 test('no throw error with different characters', () => {
     const myTeam = new Team();
